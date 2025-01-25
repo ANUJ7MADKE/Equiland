662f24d804e2764f02430f99d9e15b7d4d72489d
Manav Rathod <manav.enpointe@outlook.com>
2025-01-25 16:14:19 +0530

    grid square

--- src/components/SlidingSquare.jsx
+++ src/components/SlidingSquare.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const SlidingSquare = () => {
+const SlidingSquare = ({primaryHeading,secondaryHeading,secondaryText}) => {
   const [hovered, setHovered] = useState(false);
 
   return (
@@ -9,16 +9,14 @@ const SlidingSquare = () => {
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
-      <h1 className="text-secondary-cream text-3xl  ">
-        Primary Heading
-      </h1>
+      <h1 className="text-secondary-cream text-3xl">Primary Heading</h1>
       <div
-        className={`absolute inset-0 bg-[url('/dot-grid.svg')] bg-contain bg-repeat flex flex-col justify-between p-4 text-text-brownish bg- transition-transform duration-500 ${
+        className={`absolute inset-0 bg-[url('/dot-grid.svg')] bg-contain bg-repeat flex flex-col justify-between bg-secondary-cream p-5 text-text-brownish transition-transform duration-500  ${
           hovered ? "translate-y-0" : "-translate-y-full"
         }`}
       >
         <h2 className="text-lg font-semibold">Smaller Heading</h2>
-        <p className="text-sm">This is a description at the bottom.</p>
+        <p className="text-sm leading-[30px] font-poppins">This is a description at the bottom.</p>
       </div>
     </div>
   );
