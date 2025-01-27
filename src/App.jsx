f1ae85207366f8292509b0439ce81e25fcb8bba3
Kedar <pcbudgeofficial@gmail.com>
2025-01-27 12:35:54 +0530

    update coming soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,9 @@
-import React, { useLayoutEffect, useState } from 'react'
-import gsap from 'gsap'
-import Navbar from './components/Navbar'
-import HeroPage from './pages/HeroPage'
-import SaaSSection from './components/SaaSSection'
-import SlidingSquare from './components/SlidingSquare'
-import InsightsGrid from './components/InsightsSection'
-import MarketSection from './components/MarketSection'
-import ContactUsSection from './components/ContactUsSection'
-import Footer from './components/Footer'
-import OurJourneyPage from './pages/OurJourneyPage'
+import React from 'react'
+
 function App() {
-  const [canScroll, setCanScroll] = useState(false);
-
-  useLayoutEffect(() => {
-    // Create a GSAP context
-    const ctx = gsap.context(() => {
-      // Your GSAP animations will go here
-      gsap.from(".hero-animation", {
-        opacity: 0,
-        y: 100,
-        duration: 1,
-        stagger: 0.2,
-        ease: "power3.out",
-      });
-    });
-
-    // Cleanup
-    return () => ctx.revert();
-  }, []);
-
   return (
-    <div className='font-lexend text-text-brownish bg-secondary-cream'>
-      <Navbar canScroll={canScroll} setCanScroll={setCanScroll}/>
-      <HeroPage canScroll={canScroll} setCanScroll={setCanScroll} />
-      <SaaSSection/>
-      <InsightsGrid/>
-      <MarketSection/>
-      <OurJourneyPage />
-
-      <ContactUsSection/>
-      <Footer/>
+    <div className='min-h-screen flex items-center justify-center bg-secondary-cream'>
+      <img src="cs.png" alt="CS" className="max-w-full h-auto" />
     </div>
   );
 }
